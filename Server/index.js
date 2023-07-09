39626ee82256e784380e41ad3e8f5a04a8e98cc7
Prabh <iamprabh0001@gmail.com>
2023-07-09 20:54:16 +0530

    Initial commit

---
+++ Server/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
+
+socket.on("join_room", (data) => {
+    socket.join(data.roomName);
+    const message = `User with ID: ${socket.id} joined room: ${data.roomName}`;
+    const message2 = `User Name: ${data.userName} joined room: ${data.roomName}`;
+    console.log(message);
+    io.to(data.roomName).emit("user_joined", message2);
+  });
+
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
+
+
+
+
+ 
